f0a0457c96086ccfcaf160a58d9f9b099362e8ac
Lipan Ioan <lipanioan420@gmail.com>
2023-11-15 14:53:38 +0200

    implement child-parent communication - state logic to add/remove/check items

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Stats from "./components/Stats";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 1, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: false },
   { id: 4, description: "Toothbrush", quantity: 1, packed: false },
-  { id: 5, description: "Towel", quantity: 2, packed: true },
+  { id: 5, description: "Towel", quantity: 2, packed: false },
   { id: 6, description: "Shampoo", quantity: 1, packed: false },
 ];
 
@@ -20,11 +20,27 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Item from "./Item";
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />
         ))}
       </ul>
     </div>
