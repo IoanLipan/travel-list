21a5d460ce3dad4db20bf0823d923365a6dd9288
Lipan Ioan <lipanioan420@gmail.com>
2023-11-15 15:31:10 +0200

    remove initial items

--- src/App.js
+++ src/App.js
@@ -4,17 +4,8 @@ import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 1, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-  { id: 4, description: "Toothbrush", quantity: 1, packed: false },
-  { id: 5, description: "Towel", quantity: 2, packed: false },
-  { id: 6, description: "Shampoo", quantity: 1, packed: false },
-];
-
 export default function App() {
-  const [items, setItems] = React.useState([...initialItems]);
+  const [items, setItems] = React.useState([]);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
@@ -41,7 +32,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
