0016a7892250a70cf45a730251574bcd8c81d130
Lipan Ioan <lipanioan420@gmail.com>
2023-11-15 15:31:24 +0200

    finish footer

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <footer className="stats">
+        <em>🚀 Start adding some items to the list! 🚀</em>
+      </footer>
+    );
+  }
+
+  const totalItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.floor((100 * packedItems) / totalItems);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      {percentage === 100 ? (
+        <em>🎉🎉🎉 You're all set! 🎉🎉🎉</em>
+      ) : (
+        <em>
+          💼 You have {totalItems} items on your list, and you already packed{" "}
+          {packedItems} ({percentage}%)
+        </em>
+      )}
     </footer>
   );
 }
